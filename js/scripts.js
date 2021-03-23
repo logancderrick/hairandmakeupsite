9832107e8e6669b9c4a1c5fdf5b50b446265dc93
Logan Derrick  Intertek <logan.derrick@intertek.com>
2021-03-23 17:02:06 -0500

    Added date to booking form using datepicker plugin

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
 $(function () {
   $(".carousel").carousel({ interval: 2000 });
+
+  // date picker code
+  var date_input = $('input[name="date"]'); //our date input has the name "date"
+  var container =
+    $("#bookingForm").length > 0 ? $("#bookingForm").parent() : "body";
+  var options = {
+    format: "mm/dd/yyyy",
+    container: container,
+    todayHighlight: true,
+    autoclose: true,
+  };
+  date_input.datepicker(options);
 });
